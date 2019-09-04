d8656db5ccabd0ae4a64631b5384094598ee822f
Romulo-Reis <romuloreistavares@hotmail.com>
2019-09-04 20:15:35 -0300

    Adicionar arquivos de projeto.

---
+++ MinhaPrimeiraAplicacaoConsumirWebService/Endereco.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAplicacaoConsumirWebService
+{
+    class Endereco
+    {
+        private string descricao;
+        private string complemento;
+        private string bairro;
+        private string cidade;
+        private string uf;
+
+        public string Descricao
+        {
+            get;
+            set;
+        }
+
+        public string Complemento
+        {
+            get;
+            set;
+        }
+
+        public string Bairro
+        {
+            get;
+            set;
+        }
+        public string Cidade
+        {
+            get;
+            set;
+        }
+        public string Uf
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ MinhaPrimeiraAplicacaoConsumirWebService/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaPrimeiraAplicacaoConsumirWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cep;
+            Console.WriteLine("Bem vindo!");
+            Console.WriteLine("Favor, informe o CEP:");
+            cep = Console.ReadLine();
+            var correios = new Correios.AtendeClienteClient();
+            var consulta = correios.consultaCEP(cep.Replace("-",""));
+            Endereco endereco = new Endereco();
+            endereco.Descricao = consulta.end;
+            endereco.Complemento = consulta.complemento2;
+            endereco.Cidade = consulta.cidade;
+            endereco.Bairro = consulta.bairro;
+            endereco.Uf = consulta.uf;
+            Console.WriteLine("=================Endereço======================");
+            Console.WriteLine("Descricao:"+endereco.Descricao);
+            Console.WriteLine("Complemento:"+endereco.Complemento);
+            Console.WriteLine("Cidade:"+endereco.Cidade);
+            Console.WriteLine("Bairro:"+endereco.Bairro);
+            Console.WriteLine("UF:"+endereco.Uf);
+            Console.WriteLine("==============================================");
+            Console.ReadKey();
+
+        }
+    }
+}
